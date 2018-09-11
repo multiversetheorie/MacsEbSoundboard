7643fa778360893a1850589810093f0e8970c6c1
Ádám Kovács <35578457+multiversetheorie@users.noreply.github.com>
2018-09-11 22:44:14 +0200

    Added first sound snippet with play and pause buttons.

--- app/src/main/java/com/example/android/macsebsoundboard/MainActivity.java
+++ app/src/main/java/com/example/android/macsebsoundboard/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.macsebsoundboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Create MediaPlayer class for Repter
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.repter_korda);
+
+        //Create buttons
+        Button play_repter = findViewById(R.id.start_repter);
+        Button pause_repter = findViewById(R.id.pause_repter);
+        Button stop_repter = findViewById(R.id.stop_repter);
+
+        // Create OnClickListener for Play button
+        play_repter.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                    mediaPlayer.start();
+
+            }
+        });
+
+        // Create OnClickListener for Pause button
+        pause_repter.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+
+            }
+        });
+
+        // Create OnClickListener for Stop button
+        // TODO Implement STOP button to reset track to 0 ms and then be able to play it again
     }
 }
